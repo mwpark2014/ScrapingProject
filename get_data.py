30ed91f1411a1f4ac5fcaa57b50eb3b97e27580d
Mason <park.mason.w@gmail.com>
2017-12-18 23:15:05 -0500

    get_data returning list of elements of class=itemTitle within items with id='CircularItem-\d+' list of elements of class=itemPrice within items with id='CircularItem-\d+'

--- get_data.py
+++ get_data.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-url = input("Enter a website to extract the URL from: ")
-r = requests.get("http://" + url)
+url = "http://plan.safeway.com/Circular/Mclean-1688-Anderson-Rd/713374771/Weekly"
+r = requests.get(url)
 data = r.text
 soup = BeautifulSoup(data, 'html.parser')
 #soup = BeautifulSoup('html.parser')
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+for item in soup.find_all(id=re.compile('CircularItem-\d+')):
+    #TODO: Filter out the gift card, which has None for itemTitle and itemPrice
+    print(item.find("p", {"class": "itemTitle"}))
+    print(item.find("p", { "class" : "itemPrice" }))
+    print("\n")
