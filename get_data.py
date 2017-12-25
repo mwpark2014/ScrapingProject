7d1c569bf2f71bc0287939ea00a5be598ed3cd91
Mason <park.mason.w@gmail.com>
2017-12-25 17:31:30 -0500

    get_data now has new method getSafeway that takes in a safeway ad URL and prints item price / item title string pairs.

--- get_data.py
+++ get_data.py
@@ -2,14 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-url = "http://plan.safeway.com/Circular/Mclean-1688-Anderson-Rd/713374771/Weekly"
+#get items of specific page for Safeway ads
+def getSafeway(url:str):
+    r = requests.get(url)
+    data = r.text
+    data_html_parsed = BeautifulSoup(data, 'html.parser')
+    for item in data_html_parsed.find_all(id=re.compile('CircularItem-\d+')):
+        title = item.find("p", {"class": "itemTitle"})
+        price = item.find("p", { "class" : "itemPrice" })
+        if(title != None and price != None):
+            print(title.get_text())
+            print(price.get_text())
+        print("\n")
+
+#Find how many pages are in the Safeway local ads
+#This is the URL of the first page
+url = "http://plan.safeway.com/Circular/Mclean-1688-Anderson-Rd/713374771/Weekly/3"
 r = requests.get(url)
 data = r.text
-soup = BeautifulSoup(data, 'html.parser')
-#soup = BeautifulSoup('html.parser')
-
-for item in soup.find_all(id=re.compile('CircularItem-\d+')):
-    #TODO: Filter out the gift card, which has None for itemTitle and itemPrice
-    print(item.find("p", {"class": "itemTitle"}))
-    print(item.find("p", { "class" : "itemPrice" }))
-    print("\n")
+data_html_parsed = BeautifulSoup(data, 'html.parser')
+getSafeway(url)
+for item in data_html_parsed.find("div", {"class":"paging"}).find_all("a", title=re.compile('of')):
+    getSafeway("http://plan.safeway.com" + item.get("href"))
