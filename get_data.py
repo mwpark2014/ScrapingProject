1a95d7a651f6a871dc435e3b9c3332b3dc6e912c
Mason <park.mason.w@gmail.com>
2017-12-28 08:35:32 -0500

    get_data.py has method getSafeway to parse a safeway ad page. In the method, it uses models.py to add items to database.
    models.py imports SQLAlchemy to create object-relational mapping between database and Python objects. loadSession creates a session object, closeSession commits and flushes all changes in the session, addItem inserts new rows into table.

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from models import loadSession, addItem, closeSession
 
 #get items of specific page for Safeway ads
 def getSafeway(url:str):
@@ -13,9 +14,12 @@ def getSafeway(url:str):
         if(title != None and price != None):
             print(title.get_text())
             print(price.get_text())
+            addItem(db, title=title.get_text(), priceDesc=price.get_text())
         print("\n")
 
 #Find how many pages are in the Safeway local ads
+#Connect to database and store session in db
+db = loadSession()
 #This is the URL of the first page
 url = "http://plan.safeway.com/Circular/Mclean-1688-Anderson-Rd/713374771/Weekly/3"
 r = requests.get(url)
@@ -24,3 +28,4 @@ data_html_parsed = BeautifulSoup(data, 'html.parser')
 getSafeway(url)
 for item in data_html_parsed.find("div", {"class":"paging"}).find_all("a", title=re.compile('of')):
     getSafeway("http://plan.safeway.com" + item.get("href"))
+closeSession(db)
